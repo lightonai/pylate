5a7fc6d833ac508285ebad7409a508ddde008bd4
Antoine Chaffin <antoine.chaffin@lighton.ai>
2025-01-03 14:26:46 +0000

    Adding PyLate similarity function (maxsim) and use it as the default for ColBERT models

---
+++ pylate/scores/similarity_functions.py
@@ -0,0 +1,95 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Callable
+
+from numpy import ndarray
+from torch import Tensor
+
+from ..scores import colbert_scores, colbert_scores_pairwise
+
+
+class SimilarityFunction(Enum):
+    """
+    Enum class for supported score functions. The following functions are supported:
+
+    - ``SimilarityFunction.MAXSIM`` (``"MaxSim"``): Max similarity
+    """
+    MAXSIM = "MaxSim"
+
+
+    @staticmethod
+    def to_similarity_fn(
+        similarity_function: str | SimilarityFunction,
+    ) -> Callable[[Tensor | ndarray, Tensor | ndarray], Tensor]:
+        """
+        Converts a similarity function name or enum value to the corresponding similarity function.
+
+        Args:
+            similarity_function (Union[str, SimilarityFunction]): The name or enum value of the similarity function.
+
+        Returns:
+            Callable[[Union[Tensor, ndarray], Union[Tensor, ndarray]], Tensor]: The corresponding similarity function.
+
+        Raises:
+            ValueError: If the provided function is not supported.
+
+        Example:
+            >>> similarity_fn = SimilarityFunction.to_similarity_fn("cosine")
+            >>> similarity_scores = similarity_fn(embeddings1, embeddings2)
+            >>> similarity_scores
+            tensor([[0.3952, 0.0554],
+                    [0.0992, 0.1570]])
+        """
+        similarity_function = SimilarityFunction(similarity_function)
+        if similarity_function == SimilarityFunction.MAXSIM:
+            return colbert_scores
+
+        raise ValueError(
+            f"The provided function {similarity_function} is not supported. Use one of the supported values: {SimilarityFunction.possible_values()}."
+        )
+
+    @staticmethod
+    def to_similarity_pairwise_fn(
+        similarity_function: str | SimilarityFunction,
+    ) -> Callable[[Tensor | ndarray, Tensor | ndarray], Tensor]:
+        """
+        Converts a similarity function into a pairwise similarity function.
+
+        The pairwise similarity function returns the diagonal vector from the similarity matrix, i.e. it only
+        computes the similarity(a[i], b[i]) for each i in the range of the input tensors, rather than
+        computing the similarity between all pairs of a and b.
+
+        Args:
+            similarity_function (Union[str, SimilarityFunction]): The name or enum value of the similarity function.
+
+        Returns:
+            Callable[[Union[Tensor, ndarray], Union[Tensor, ndarray]], Tensor]: The pairwise similarity function.
+
+        Raises:
+            ValueError: If the provided similarity function is not supported.
+
+        Example:
+            >>> pairwise_fn = SimilarityFunction.to_similarity_pairwise_fn("cosine")
+            >>> similarity_scores = pairwise_fn(embeddings1, embeddings2)
+            >>> similarity_scores
+            tensor([0.3952, 0.1570])
+        """
+        similarity_function = SimilarityFunction(similarity_function)
+        if similarity_function == SimilarityFunction.MAXSIM:
+            return colbert_scores_pairwise
+        
+    @staticmethod
+    def possible_values() -> list[str]:
+        """
+        Returns a list of possible values for the SimilarityFunction enum.
+
+        Returns:
+            list: A list of possible values for the SimilarityFunction enum.
+
+        Example:
+            >>> possible_values = SimilarityFunction.possible_values()
+            >>> possible_values
+            ['MaxSim']
+        """
+        return [m.value for m in SimilarityFunction]
