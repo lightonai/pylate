35722d12ef8bed26cd0052d73b0af17c5ad4e236
Antoine Chaffin <antoine.chaffin@lighton.ai>
2025-05-16 09:03:31 +0000

    Add loading tests for PLAID

--- tests/test_plaid.py
+++ tests/test_plaid.py
@@ -2,9 +2,11 @@ import uuid
 
 from pylate import indexes, models
 
+# We have using random match to not conflict with parallel tests
+random_hash = uuid.uuid4().hex
 index = indexes.PLAID(
-    index_folder=f"test_indexes_{uuid.uuid4().hex}",
-    index_name=f"colbert_{uuid.uuid4().hex}",
+    index_folder=f"test_indexes_{random_hash}",
+    index_name=f"colbert_{random_hash}",
     override=True,
 )
 
@@ -33,7 +35,9 @@ matchs = index(queries_embeddings, k=30)
 
 assert isinstance(matchs, list)
 assert len(matchs) == 2
+assert len(matchs[0]) == 2
 assert matchs[0][0].keys() == {"id", "score"}
+
 
 queries_embeddings = model.encode(
     "fruits are healthy.",
@@ -44,4 +48,42 @@ matchs = index(queries_embeddings, k=30)
 
 assert isinstance(matchs, list)
 assert len(matchs) == 1
+assert len(matchs[0]) == 2
+assert matchs[0][0].keys() == {"id", "score"}
+
+# Test loading
+index = indexes.PLAID(
+    index_folder=f"test_indexes_{random_hash}",
+    index_name=f"colbert_{random_hash}",
+    override=False,
+)
+
+matchs = index(queries_embeddings, k=30)
+assert isinstance(matchs, list)
+assert len(matchs) == 1
+assert len(matchs[0]) == 2
+assert matchs[0][0].keys() == {"id", "score"}
+
+# Test removing documents
+index.remove_documents(
+    documents_ids=["1"],
+)
+
+matchs = index(queries_embeddings, k=30)
+assert isinstance(matchs, list)
+assert len(matchs) == 1
+assert len(matchs[0]) == 1
+assert matchs[0][0].keys() == {"id", "score"}
+
+
+# Test second insertion after init of the index
+index.add_documents(
+    documents_ids=["1"],
+    documents_embeddings=documents_embeddings[0],
+)
+
+matchs = index(queries_embeddings, k=30)
+assert isinstance(matchs, list)
+assert len(matchs) == 1
+assert len(matchs[0]) == 2
 assert matchs[0][0].keys() == {"id", "score"}
