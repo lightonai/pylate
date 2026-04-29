fa4f356c28fc2226a34fda8774fb61a824f8c539
Antoine Chaffin <ant54600@hotmail.fr>
2026-04-29 13:56:27 +0000

    remove the export from Base index

--- pylate/indexes/__init__.py
+++ pylate/indexes/__init__.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from .base import Base
 from .plaid import PLAID
 from .scann import ScaNN
 from .voyager import Voyager
 
-__all__ = ["Base", "Voyager", "PLAID", "ScaNN"]
+__all__ = ["Voyager", "PLAID", "ScaNN"]
